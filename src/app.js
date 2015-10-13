4b1476c702014b80db5cfdbcb55e938d6a64c5cb
U-KCURA\aarsan <aarsan@P-DV-DSK-AARS.kcura.corp>
2015-10-13 18:52:21 -0500

    working on routes

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-var app = angular.module('citycorner', []);
+var app = angular.module('CityCornerModule', []);
+
+CityCornerModule.config(['$routeProvider, function ($routeProvider) {
+    $routeProvider.when('/login', {
+      templateUrl: 'views/login.html',
+      controller: 'LoginCtrl'
+    });
+
+    $routeProvider.otherwise({
+      templateUrl: 'views/404.html'
+    });
+}]);
